33bcb53d2827c98bebf2e3789df6b5a9324ff2db
wasan <ameera19x@gmail.com>
2026-02-27 21:52:14 +0300

    Initial Commit

---
+++ Shafrah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shafrah
+//
+//  Created by wasan hamoud on 10/09/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shafrah/ShafrahApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShafrahApp.swift
+//  Shafrah
+//
+//  Created by wasan hamoud on 10/09/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct ShafrahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
